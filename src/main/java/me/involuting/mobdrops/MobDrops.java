a6399b0a8ff5ee1a775323050fdee1f746e1ec57
doricns <tovl68025@gmail.com>
2026-04-26 12:39:08 +0930

    Init

--- src/main/java/me/involuting/mobdrops/MobDrops.java
+++ src/main/java/me/involuting/mobdrops/MobDrops.java
@@ -1,12 +1,15 @@
 package me.involuting.mobdrops;
 
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
-
+@Getter
 public final class MobDrops extends JavaPlugin {
+    private MobDrops instance;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
+
 
     }
 
